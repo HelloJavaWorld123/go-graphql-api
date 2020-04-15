6f57c92b3d4155ac6a4f127f73d41b96d15c43b8
HelloJavaWorld123 <1806146700@qq.com>
2020-04-15 23:51:59 +0800

    增加main文件的引用

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"errors"
+	"github.com/HelloJavaWorld123/go-graphql-api/postgres"
+)
+
 func main() {
 
+}
+
+func initializeApi() *postgres.Db {
+	panic(errors.New("initializeApi"))
 }
